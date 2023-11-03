4452928cc7b916f7ef54edb6ee5287dab21829cc
Mario Márquez <mariomarquez.0116@gmail.com>
2023-11-03 12:47:42 -0400

    Feat: Saldo añadido al .csv, finalizado la seccion 1 y subir la informacion al .csv implementado.

--- Sistema_Bancario.cpp
+++ Sistema_Bancario.cpp
@@ -5,11 +5,11 @@
 #include <fstream>
 using namespace std;
 struct Clientes{
-    int ci, saldo, numero_de_penalizaciones;
+    int ci, numero_de_penalizaciones, saldo;
     string nombre, tipo_de_cuenta, suspension, numero_de_cuenta;
 };
-int flag=0, search_ci, search_ci_2, deposito=0, retiro=0, search_numero_de_cuenta_2, search_numero_de_cuenta_3; 
-string search_numero_de_cuenta;
+int flag=0, flag_2=0, search_ci, search_ci_2, deposito=0, retiro=0, search_numero_de_cuenta_2, search_numero_de_cuenta_3, search_nombre_2; 
+string search_numero_de_cuenta, search_nombre, saldo[1000];
 int main(){
     Clientes cliente[2000]; 
     string line, word;
@@ -27,7 +27,8 @@ int main(){
                 case 1: cliente[nline].nombre=word;
                 case 2: cliente[nline].numero_de_cuenta=word;
                 case 3: cliente[nline].tipo_de_cuenta=word;
-                case 4: cliente[nline].suspension=word;
+                case 4: saldo[nline]=word;
+                case 5: cliente[nline].suspension=word;
             }
             count++;
         }
@@ -35,34 +36,101 @@ int main(){
         nline++;
     }
     in_file.close();
-    do{
-        cout<<"Bienvenido a la plataforma online de nuestro banco, a continuacion se le dara las siguientes opciones:\n1)Consulta de cliente.\n2)Busqueda de cliente.\n3)Operaciones bancarias.\n4)Estado de penalizaciones de su cuenta.\n5)Salir del programa.\nElija su opcion: "; cin>>flag;
+    for(int i=0; i<1000; i++){
+        cliente[i].saldo=stoi(saldo[i]); //Hice muchas pruebas, y por mas que los datos se estaban guardando bien, no pude lograr guardar el saldo en la parte anterior debido a un error con el stoi
+    }do{
+        cout<<"Bienvenido a la plataforma online de nuestro banco, a continuacion se le dara las siguientes opciones:\n1)Consulta de cliente.\n2)Operaciones bancarias.\n3)Estado de suspension de su cuenta.\n4)Salir del programa.\nElija su opcion: "; cin>>flag;
         system("cls");
         switch(flag){
             case 1:
-                cout<<"Bienvenido a la consulta de su estado de cuenta.\nNecesita ingresar su numero de cedula de identidad para poder visualizar su estado de cuenta.\nIngrese su cedula: "; cin>>search_ci;
-                system("cls");
-                for(int i=0; i<1000; i++){ //Razonamiento para usar linear search: Ya que solo se usara para buscar un cliente y no para filtrar en un cierto orden, no vi necesidad de implementar un sistema de ordenamiento para utilizar los otros metodos de busquedas, ya que se usaran en la seccion 2 de este programa.
-                    if(search_ci==cliente[i].ci){
-                        cout<<"El cliente es: "<<cliente[i].nombre<<endl;
-                        cout<<"El numero de cuenta es: "<<cliente[i].numero_de_cuenta<<endl;
-                        cout<<"El tipo de cuenta es: "<<cliente[i].tipo_de_cuenta<<endl;
-                        cout<<"El saldo de la cuenta es: "<<cliente[i].saldo<<endl;
-                        system("pause");
+                do{
+                    cout<<"Bienvenido a la seccion para consultar su estado de cuenta, a continuacion se le dira sus opciones:\n1)Buscar su cuenta mediante su nombre y numero de cuenta.\n2)Buscar su cuenta mediante su cedula y numero de cuenta.\n3)Volver al menu principal.\nElija su opcion: "; cin>>flag_2;
+                    system("cls");
+                    switch(flag_2){
+                        case 1:
+                        rewind(stdin);
+                        cout<<"Por favor ingrese su nombre y apellido: "; getline(cin, search_nombre);
                         system("cls");
+                        rewind(stdin);
+                        cout<<"Ingrese su numero de cuenta: "; getline(cin, search_numero_de_cuenta);
+                        system("cls");
+                        rewind(stdin);
+                        for(int i=0; i<1000; i++){ //Mismo razonamiento que la seccion 1
+                            if(search_nombre==cliente[i].nombre){
+                                search_nombre_2=i;
+                                break;
+                            }
+                        }for(int i=0; i<1000; i++){ 
+                            if(search_numero_de_cuenta==cliente[i].numero_de_cuenta){
+                                search_numero_de_cuenta_2=i;
+                                break;
+                            }
+                        }if(search_numero_de_cuenta_2!=search_nombre_2){
+                            cout<<"Los datos ingresados no coinciden, por favor vuelva a intentar"<<endl;
+                            system("pause");
+                            system("cls");
+                        }else{
+                            cout<<"Bienvenido/a "<<cliente[search_nombre_2].nombre<<", este es su estado de cuenta: "<<endl;
+                            cout<<"Su saldo actual: "<<cliente[search_nombre_2].saldo<<endl;
+                            cout<<"Informacion adicional: "<<endl;
+                            cout<<"Numero de cedula: "<<cliente[search_nombre_2].ci<<endl;
+                            cout<<"Numero de cuenta: "<<cliente[search_nombre_2].numero_de_cuenta<<endl;
+                            cout<<"Tipo de cuenta: "<<cliente[search_nombre_2].tipo_de_cuenta<<endl;
+                            cout<<"Para mas informacion sobre el estado de suspension, por favor vaya a la seccion Estado de suspension de su cuenta, en el menu principal"<<endl;
+                            system("pause");
+                            system("cls");
+                        }
                         break;
-                    }else if(search_ci!=cliente[i].ci && i==999){
-                        cout<<"La cedula que ingreso no es correcta, por favor vuelva a intentarlo"<<endl;
+                        case 2:
+                        cout<<"Por favor ingrese el numero de su cedula de indentidad: "; cin>>search_ci;
+                        system("cls");
+                        rewind(stdin);
+                        cout<<"Ingrese su numero de cuenta: "; getline(cin, search_numero_de_cuenta);
+                        system("cls");
+                        rewind(stdin);
+                        for(int i=0; i<1000; i++){ //Mismo razonamiento que la seccion 1
+                            if(search_ci==cliente[i].ci){
+                                search_ci=i;
+                                break;
+                            }
+                        }for(int i=0; i<1000; i++){ 
+                            if(search_numero_de_cuenta==cliente[i].numero_de_cuenta){
+                                search_numero_de_cuenta_2=i;
+                                break;
+                            }
+                        }if(search_numero_de_cuenta_2!=search_ci){
+                            cout<<"Los datos ingresados no coinciden, por favor vuelva a intentar"<<endl;
+                            system("pause");
+                            system("cls");
+                        }else{
+                            cout<<"Bienvenido/a "<<cliente[search_ci].nombre<<", este es su estado de cuenta: "<<endl;
+                            cout<<"Su saldo actual: "<<cliente[search_ci].saldo<<endl;
+                            cout<<"Informacion adicional: "<<endl;
+                            cout<<"Numero de cedula: "<<cliente[search_ci].ci<<endl;
+                            cout<<"Numero de cuenta: "<<cliente[search_ci].numero_de_cuenta<<endl;
+                            cout<<"Tipo de cuenta: "<<cliente[search_ci].tipo_de_cuenta<<endl;
+                            cout<<"Para mas informacion sobre el estado de suspension, por favor vaya a la seccion Estado de suspension de su cuenta, en el menu principal"<<endl;
+                            system("pause");
+                            system("cls");
+                        }
+                        break;
+                        case 3:
+                            cout<<"Esperamos que haya sido fructifera su consulta!"<<endl;
+                            system("pause");
+                            system("cls");
+                            break;
+                        default:
+                        cout<<"Ingrese una opcion valida."<<endl;
                         system("pause");
                         system("cls");
                     }
-                }
+                }while(flag_2!=3);
                 break;
-            case 3:
+            case 2:
                 do{
-                    cout<<"Bienvenido al sistema de operaciones bancarias, a continuacion se le dira sus opciones:\n1)Deposito.\n2)Retiro.\n3)Transferencia entre usuarios de nuestro mismo banco.\n4)Volver al menu principal.\nElija su opcion: "; cin>>flag;
+                    cout<<"Bienvenido al sistema de operaciones bancarias, a continuacion se le dira sus opciones:\n1)Deposito.\n2)Retiro.\n3)Transferencia entre usuarios de nuestro mismo banco.\n4)Volver al menu principal.\nElija su opcion: "; cin>>flag_2;
                     system("cls");
-                    switch(flag){
+                    switch(flag_2){
                         case 1:
                         cout<<"Bienvenido a la seccion de depositos.\nPor favor ingrese el numero de su cedula de indentidad: "; cin>>search_ci;
                         system("cls");
@@ -269,9 +337,9 @@ int main(){
                             system("cls");
                             break;
                     }
-                }while(flag!=4);
+                }while(flag_2!=4);
             break;
-            case 4:
+            case 3:
                 cout<<"Bienvenido a la seccion de penalizacion de su cuenta, a continuacion se le nombrara las politicas del banco que en caso de incumplirse sera motivo de penalizacion.\n1)Ingresar montos en las operaciones iguales a 0 o menor.\n2)Realizar retiros cuyo monto exceda los 50.000$ en una sola operacion.\n3)Realizar transferencias entre clientes del mismo banco cuyo monto exceda los 100.000$ en una sola operacion.\nEn caso de recibir 3 penalizaciones, su cuenta sera suspendida hasta nuevo aviso.\nPor favor ingrese su numero de cedula para verificar el estado de suspension de su cuenta: "; cin>>search_ci;
                 system("cls");
                 for(int i=0; i<1000; i++){ //Mismo razonamiento que la seccion 1
@@ -294,7 +362,7 @@ int main(){
                         system("cls");
                     }
                     break;
-                case 5:
+                case 4:
                     cout<<"Gracias por su confianza, vuelva pronto!"<<endl;
                     system("pause");
                     system("cls");
@@ -305,6 +373,11 @@ int main(){
                     system("cls");
                     break;
         }
-    }while(flag!=5);
+    }while(flag!=4);
+    ofstream write_file("clients.csv", ofstream::out);
+    for(int i=0; i<1000; i++){
+        write_file<<cliente[i].ci<<","<<cliente[i].nombre<<","<<cliente[i].numero_de_cuenta<<","<<cliente[i].tipo_de_cuenta<<","<<cliente[i].saldo<<","<<cliente[i].suspension<<endl;
+    }
+    write_file.close();
     return 0;
 }
